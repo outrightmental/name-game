46e1385308fdd0fea6a0e17d5c4957edbd049491
Charney Kaye <charney@outright.io>
2017-06-17 15:03:47 -0400

    First version of name tester

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 
+function render() {
+	$('.name').html(getName());
+	$('.description').html(getDescription());
+	$('.tagline').html(getTagline());
+}
+
+function capitalized(text) {
+	return text.substr(0,1).toUpperCase()+text.substr(1);
+}
+
+function getName() {
+	return capitalized($('#testName').val());
+}
+
+function getDescription() {
+	return $('#testDescription').val();
+}
+
+function getTagline() {
+	return $('#testTagline').val();
+}
